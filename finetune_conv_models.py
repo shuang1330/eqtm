a012f08b86c8aa998c2fb0c77b23a55e95f4f83b
shuang <shuangli1330@gmail.com>
2018-04-30 09:33:07 +0200

    .gitignore
    ignore bed files

--- finetune_conv_models.py
+++ finetune_conv_models.py
@@ -98,7 +98,8 @@ def read_gavin(gavin_res, labels):
     specificity_g = tn_g/(fp_g+tn_g)
     return sensitivity_g, specificity_g
 
-def display_res_gavin_and_best_model(param_grid,pipeline,mvid,filename=None):
+def display_res_gavin_and_best_model(param_grid,pipeline,mvid,
+filename=None,returnRes = False):
     '''
     use model defined by pipeline to fit mvid Dataset
     gridsearchCV determine the parameters given in param_grid
@@ -116,6 +117,8 @@ def display_res_gavin_and_best_model(param_grid,pipeline,mvid,filename=None):
     sensitivity,specificity,score = run_display_output(classifier,mvid.test)
     print('>>> best model results: sensitivity: {:.{prec}}\tspecificity: {:.{prec}f}\tauc:{}'.\
     format(sensitivity,specificity,score,prec=3))
+    if returnRes:
+        return sensitivity,specificity,score,classifier
     return classifier
 
 def display_res_gavin_and_elasticnet(param_grid,pipeline,mvid,filename=None):
@@ -138,102 +141,169 @@ def display_res_gavin_and_elasticnet(param_grid,pipeline,mvid,filename=None):
     format(sensitivity,specificity,score,prec=3))
     return classifier
 
+def load_bonderWestraData(path):
+    data = pd.read_csv(path,sep=',')
+    # print(data.head())
+    def binarize(row):
+        if row > 0:
+            return 1
+        else:
+            return 0
+    data['direction'] = data['zscore'].apply(binarize)
+    dataset = read_data_set(data)
+    return dataset
+
+def calculateKnnVariance(bonder_path):
+    res = {'sensitivity':[],'specificity':[],'auc':[]}
+    for i in range(4):
+        eqtm_data = load_bonderWestraData(bonder_path)
+        pipeline_kneighbor = Pipeline(steps=[('kneighbor',
+                                      KNeighborsClassifier())])
+        n_neighbors = range(2,10)
+        weights = ['uniform','distance']
+        # algorithms = ['auto']
+        # leaf_size = [20,30,40,50]
+        # p = [1,2,3]
+        param_grid_kneighbor = [{'kneighbor__n_neighbors':n_neighbors,
+                              'kneighbor__weights':weights}]#,
+                              # 'kneighbor__algorithms':algorithms}]#,
+                              # 'kneighbor__leaf_size':leaf_size,
+                              # 'kneighbor__p':p}]
+        sensitivity,specificity,auc,classifier_kneighbor = \
+        display_res_gavin_and_best_model(
+                                         param_grid_kneighbor,
+                                         pipeline_kneighbor,
+                                         eqtm_data,
+                                         returnRes=True)
+        res['sensitivity'].append(sensitivity)
+        res['specificity'].append(specificity)
+        res['auc'].append(auc)
+    print('Sensitivity:',
+    np.mean(np.array(res['sensitivity'])),
+    np.std(np.array(res['sensitivity'])))
+    print('Specificity:',
+    np.mean(np.array(res['specificity'])),
+    np.std(np.array(res['specificity'])))
+    print('AUC:',
+    np.mean(np.array(res['auc'])),
+    np.std(np.array(res['auc'])))
+
+def calculateRanforVariance(bonder_path):
+    res = {'sensitivity':[],'specificity':[],'auc':[]}
+    for i in range(4):
+        eqtm_data = load_bonderWestraData(bonder_path)
+        pipeline_ranfor = Pipeline(steps=[('pca',PCA()),('ranfor',
+                                           RandomForestClassifier())])
+        n_estimators = [10,50,100]
+        class_weight = ['balanced',{1:4,0:1},{1:2,0:1}]
+        n_components = np.arange(2,eqtm_data.train.num_features,10)
+        param_grid_ranfor = [{'pca__n_components':n_components,
+                              'ranfor__n_estimators':n_estimators,
+                              'ranfor__class_weight':class_weight}]
+        sensitivity,specificity,auc,classifier_ranfor = \
+        display_res_gavin_and_best_model(
+                                         param_grid_ranfor,
+                                         pipeline_ranfor,
+                                         eqtm_data,
+                                         returnRes=True)
+        res['sensitivity'].append(sensitivity)
+        res['specificity'].append(specificity)
+        res['auc'].append(auc)
+    print('Sensitivity:',
+    np.mean(np.array(res['sensitivity'])),
+    np.std(np.array(res['sensitivity'])))
+    print('Specificity:',
+    np.mean(np.array(res['specificity'])),
+    np.std(np.array(res['specificity'])))
+    print('AUC:',
+    np.mean(np.array(res['auc'])),
+    np.std(np.array(res['auc'])))
+
 if __name__=='__main__':
 
-    def load_bonderWestraData(path):
-        data = pd.read_csv(path,sep=',')
-        # print(data.head())
-        def binarize(row):
-            if row > 0:
-                return 1
-            else:
-                return 0
-        data['direction'] = data['zscore'].apply(binarize)
-        dataset = read_data_set(data)
-        return dataset
+    bonder_path = 'data/Newbonder_withzscore.csv'
+    westra_allFeat_path = 'data/Newwestra_all_with_zscore.csv'
+    westra_bonderFeat_path = 'data/Newwestra_bonderfeat_with_zscore.csv'
 
+    # bonder = load_bonderWestraData(bonder_path)
+    # westra_allFeat = load_bonderWestraData(westra_allFeat_path)
+    # westra_bonderFeat = load_bonderWestraData(westra_bonderFeat_path)
 
-    bonder_path = 'data/bonder_withzscore.csv'
-    westra_allFeat_path = 'data/westra_all_with_zscore.csv'
-    westra_bonderFeat_path = 'data/westra_bonderfeat_with_zscore.csv'
+    # print('Bonder dataset loaded.',bonder.train.values.shape)
+    # print('Westra with all features dataset loaded.',westra_allFeat.train.values.shape)
+    # print('Westra with bonder features dataset loaded.',
+           # westra_bonderFeat.train.values.shape)
 
-    bonder = load_bonderWestraData(bonder_path)
-    westra_allFeat = load_bonderWestraData(westra_allFeat_path)
-    westra_bonderFeat = load_bonderWestraData(westra_bonderFeat_path)
-
-    print('Bonder dataset loaded.',bonder.train.values.shape)
-    print('Westra with all features dataset loaded.',westra_allFeat.train.values.shape)
-    print('Westra with bonder features dataset loaded.',
-           westra_bonderFeat.train.values.shape)
-
-    for eqtm_data in [bonder,westra_allFeat,westra_bonderFeat]:
-            print('Dataset:', eqtm_data.train.values.shape)
-        # ================LogisticRegression====================================
-            n_components = np.arange(2,eqtm_data.train.num_features,10)
-            class_weight = ['balanced',{1:4,0:1},{1:2,0:1}]
-            param_grid_logr = [{'logr__penalty':['l1','l2'],
-                                'logr__C':[1,2,3,4,5],
-                                'logr__class_weight':class_weight}]
-            # pipeline
-            pipeline_logr = Pipeline(steps=[('logr',LogisticRegression())])
-            classifier_logr = display_res_gavin_and_best_model(param_grid_logr,
-                                             pipeline_logr,
-                                             eqtm_data)
-        # ====================randomForest======================================
-            pipeline_ranfor = Pipeline(steps=[('ranfor',
-                                       RandomForestClassifier())])
-            n_estimators = [10,50,100]
-            class_weight = ['balanced',{1:4,0:1},{1:2,0:1}]
-            param_grid_ranfor = [{'ranfor__n_estimators':n_estimators,
-                                  'ranfor__class_weight':class_weight}]
-            classifier_ranfor = display_res_gavin_and_best_model(
-                                             param_grid_ranfor,
-                                             pipeline_ranfor,
-                                             eqtm_data)
-        # ====================k-nearest neighbor================================
-            pipeline_kneighbor = Pipeline(steps=[('kneighbor',
-                                          KNeighborsClassifier())])
-            # print(pipeline_kneighbor.get_params().keys())
-            n_neighbors = range(2,10)
-            weights = ['uniform','distance']
-            # algorithms = ['auto']
-            # leaf_size = [20,30,40,50]
-            # p = [1,2,3]
-            param_grid_kneighbor = [{'kneighbor__n_neighbors':n_neighbors,
-                                  'kneighbor__weights':weights}]#,
-                                  # 'kneighbor__algorithms':algorithms}]#,
-                                  # 'kneighbor__leaf_size':leaf_size,
-                                  # 'kneighbor__p':p}]
-            classifier_kneighbor = display_res_gavin_and_best_model(
-                                             param_grid_kneighbor,
-                                             pipeline_kneighbor,
-                                             eqtm_data)
-        # ====================gaussian_process==================================
-            pipeline_gaussian = Pipeline(steps=[('gaussian',
-                                         GaussianProcessClassifier())])
-            param_grid_gaussian = [{'gaussian__max_iter_predict':[50]}]
-            classifier_gaussian = display_res_gavin_and_best_model(
-                                             param_grid_gaussian,
-                                             pipeline_gaussian,
-                                             eqtm_data)
-
-        # ====================SVM===============================================
-            pipeline_svc = Pipeline(steps=[('svc',SVC())])
-            C = [0.5,1,2,3]
-            kernel = ['linear','poly','rbf','sigmoid']
-            class_weight = ['balanced',{1:4,0:1},{1:2,0:1}]
-            probability = [True]
-            param_grid_svc = [{'svc__kernel':kernel,
-                               'svc__class_weight':class_weight,
-                               'svc__probability':probability}]
-            classifier_svc = display_res_gavin_and_best_model(param_grid_svc,
-                                             pipeline_svc,
-                                             eqtm_data)
-        # ====================Adaboost==========================================
-            pipeline_ada = Pipeline(steps=[('ada',AdaBoostClassifier())])
-            base_estimator = [DecisionTreeClassifier()]
-            param_grid_ada = [{'ada__base_estimator':base_estimator}]
-            classifier_ada = display_res_gavin_and_best_model(
-                                             param_grid_ada,
-                                             pipeline_ada,
-                                             eqtm_data)
+    # calculateKnnVariance(bonder_path)
+    calculateRanforVariance(bonder_path)
+    # for eqtm_data in [bonder,westra_allFeat,westra_bonderFeat]:
+    #         print('Dataset:', eqtm_data.train.values.shape)
+    #     # ================LogisticRegression====================================
+    #         n_components = np.arange(2,eqtm_data.train.num_features,10)
+    #         class_weight = ['balanced',{1:4,0:1},{1:2,0:1}]
+    #         param_grid_logr = [{'logr__penalty':['l1','l2'],
+    #                             'logr__C':[1,2,3,4,5],
+    #                             'logr__class_weight':class_weight}]
+    #         # pipeline
+    #         pipeline_logr = Pipeline(steps=[('logr',LogisticRegression())])
+    #         classifier_logr = display_res_gavin_and_best_model(param_grid_logr,
+    #                                          pipeline_logr,
+    #                                          eqtm_data)
+    #     # ====================randomForest======================================
+    #         pipeline_ranfor = Pipeline(steps=[('ranfor',
+    #                                    RandomForestClassifier())])
+    #         n_estimators = [10,50,100]
+    #         class_weight = ['balanced',{1:4,0:1},{1:2,0:1}]
+    #         param_grid_ranfor = [{'ranfor__n_estimators':n_estimators,
+    #                               'ranfor__class_weight':class_weight}]
+    #         classifier_ranfor = display_res_gavin_and_best_model(
+    #                                          param_grid_ranfor,
+    #                                          pipeline_ranfor,
+    #                                          eqtm_data)
+    #     # ====================k-nearest neighbor================================
+    #         pipeline_kneighbor = Pipeline(steps=[('kneighbor',
+    #                                       KNeighborsClassifier())])
+    #         # print(pipeline_kneighbor.get_params().keys())
+    #         n_neighbors = range(2,10)
+    #         weights = ['uniform','distance']
+    #         # algorithms = ['auto']
+    #         # leaf_size = [20,30,40,50]
+    #         # p = [1,2,3]
+    #         param_grid_kneighbor = [{'kneighbor__n_neighbors':n_neighbors,
+    #                               'kneighbor__weights':weights}]#,
+    #                               # 'kneighbor__algorithms':algorithms}]#,
+    #                               # 'kneighbor__leaf_size':leaf_size,
+    #                               # 'kneighbor__p':p}]
+    #         classifier_kneighbor = display_res_gavin_and_best_model(
+    #                                          param_grid_kneighbor,
+    #                                          pipeline_kneighbor,
+    #                                          eqtm_data)
+    #     # ====================gaussian_process==================================
+    #         pipeline_gaussian = Pipeline(steps=[('gaussian',
+    #                                      GaussianProcessClassifier())])
+    #         param_grid_gaussian = [{'gaussian__max_iter_predict':[50]}]
+    #         classifier_gaussian = display_res_gavin_and_best_model(
+    #                                          param_grid_gaussian,
+    #                                          pipeline_gaussian,
+    #                                          eqtm_data)
+    #
+    #     # ====================SVM===============================================
+    #         pipeline_svc = Pipeline(steps=[('svc',SVC())])
+    #         C = [0.5,1,2,3]
+    #         kernel = ['linear','poly','rbf','sigmoid']
+    #         class_weight = ['balanced',{1:4,0:1},{1:2,0:1}]
+    #         probability = [True]
+    #         param_grid_svc = [{'svc__kernel':kernel,
+    #                            'svc__class_weight':class_weight,
+    #                            'svc__probability':probability}]
+    #         classifier_svc = display_res_gavin_and_best_model(param_grid_svc,
+    #                                          pipeline_svc,
+    #                                          eqtm_data)
+    #     # ====================Adaboost==========================================
+    #         pipeline_ada = Pipeline(steps=[('ada',AdaBoostClassifier())])
+    #         base_estimator = [DecisionTreeClassifier()]
+    #         param_grid_ada = [{'ada__base_estimator':base_estimator}]
+    #         classifier_ada = display_res_gavin_and_best_model(
+    #                                          param_grid_ada,
+    #                                          pipeline_ada,
+    #                                          eqtm_data)

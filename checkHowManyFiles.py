891dd853376ee3729efe5d3730f15f7ca1e19487
shuang <shuangli1330@gmail.com>
2018-05-15 10:08:11 +0200

    check the status of files processed

---
+++ checkHowManyFiles.py
@@ -0,0 +1,11 @@
+import os
+
+# path = '/groups/umcg-gcc/tmp03/umcg-sli/eqtm_project/data/temp/overlap/random20000-eQTLsFDR-gt0.05-flipped'
+# path2 = '/groups/umcg-gcc/tmp03/umcg-sli/eqtm_project/data/temp/features'
+# path3 = '/groups/umcg-wijmenga/tmp03/projects/eQTMPrediction/features/Roadmap/consolidatedImputedGappedPeak'
+# print(len(os.listdir(path)))
+# print(len(os.listdir(path2)))
+# print(len(os.listdir(path3)))
+
+path = ''
+print(len(os.listdir(path)))

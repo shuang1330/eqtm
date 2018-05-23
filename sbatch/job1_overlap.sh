908eb465d7f3c8f7513ae0fcbe00ec3192120f02
shuang <shuangli1330@gmail.com>
2018-05-23 16:42:43 +0200

    jobs in boxy

---
+++ sbatch/job1_overlap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --ntasks=2
+#SBATCH --time=05:00:00
+#SBATCH --job-name=job1Overlap
+#SBATCH --output=gt0.05_sm0.1.out
+
+module load TensorFlow/1.5.0-foss-2015b-Python-3.6.3
+module load BEDTools/2.25.0-foss-2015b
+cd /groups/umcg-gcc/tmp03/umcg-sli/eqtm_project
+# python ./boxy_tools/buildOverlapRatioTable_boxy.py --rootDir /groups/umcg-gcc/tmp03/umcg-sli/eqtm_project --featureRootDir /groups/umcg-bios/tmp03/projects/2018-methylation/input --cpg random20k_gt0.5 --bedtools findOverlap
+python finalOverlapMatrixjob1.py
---
+++ sbatch/jobList/job3_002_003.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --ntasks=2
+#SBATCH --time=09:00:00
+#SBATCH --job-name=testjob
+
+module load TensorFlow/1.5.0-foss-2015b-Python-3.6.3
+module load BEDTools/2.25.0-foss-2015b
+cd /groups/umcg-gcc/tmp03/umcg-sli/eqtm_project
+python ./boxy_tools/buildOverlapRatioTable_boxy.py --rootDir /groups/umcg-gcc/tmp03/umcg-sli/eqtm_project --featureRootDir /groups/umcg-bios/tmp03/projects/2018-methylation/input --cpg random20k_gt0.2_sm0.3 --bedtools findOverlap

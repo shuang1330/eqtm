d6c2a5d40ce031e31278301c52751c11daddc091
shuang <shuangli1330@gmail.com>
2018-05-23 16:45:49 +0200

    added some paths to check

--- checkHowManyFiles.py
+++ checkHowManyFiles.py
@@ -1,11 +1,11 @@
 import os
 
-# path = '/groups/umcg-gcc/tmp03/umcg-sli/eqtm_project/data/temp/overlap/random20000-eQTLsFDR-gt0.05-flipped'
-# path2 = '/groups/umcg-gcc/tmp03/umcg-sli/eqtm_project/data/temp/features'
-# path3 = '/groups/umcg-wijmenga/tmp03/projects/eQTMPrediction/features/Roadmap/consolidatedImputedGappedPeak'
-# print(len(os.listdir(path)))
-# print(len(os.listdir(path2)))
-# print(len(os.listdir(path3)))
-
-path = ''
+path = '/groups/umcg-gcc/tmp03/umcg-sli/eqtm_project/data/temp/features/random20k_gt0.5'
+path2 = '/groups/umcg-gcc/tmp03/umcg-sli/eqtm_project/data/temp/features'
+path3 = '/groups/umcg-bios/tmp03/projects/2018-methylation/input/features/Roadmap/consolidatedImputedGappedPeak'
 print(len(os.listdir(path)))
+print(len(os.listdir(path2)))
+print(len(os.listdir(path3)))
+
+# path = ''
+# print(len(os.listdir(path)))
